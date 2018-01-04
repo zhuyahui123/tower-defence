2b7c78d5c3837d6002b7165f7c278fac1cb232d4
huizhu123 <zyhxhu@163.com>
2018-01-04 11:01:20 +0800

    添加倒计时显示标识

--- assets/scripts/game-ui-layer.js
+++ assets/scripts/game-ui-layer.js
@@ -4,11 +4,13 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        timeCountDownLabel: {displayName: "倒计时", default: null, type: cc.Label}
+        timeCountDownLabel: {displayName: "倒计时", default: null, type: cc.Label},
+        startLabel: {displayName: "游戏开始", default: null, type: cc.Label}
     },
 
     onLoad() {
         this.nowTime = 4;
+        this.startLabel.node.active = false;
     },
     update(dt) {
         if (this.nowTime > 0) {
@@ -19,6 +21,13 @@ cc.Class({
                 if (Math.floor(this.nowTime) === 0) {
                     cc.log("游戏开始");
                     this.timeCountDownLabel.string = "";
+                    this.startLabel.node.active = true;
+                    let action0 = cc.moveTo(0.2, cc.p(0, 0));
+                    this.startLabel.node.runAction(action0);
+                    this.scheduleOnce(function () {
+                        let action1 = cc.fadeOut(1.0);
+                        this.startLabel.node.runAction(action1);
+                    }, 1);
                     this.nowTime = 0;
                     global.event.fire("game_start"); //发一个游戏开始消息
                 }
