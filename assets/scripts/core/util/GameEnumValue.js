024ed27bb19fc45bb9255eb6eb79e558121dbdda
huizhu123 <zyhxhu@163.com>
2018-01-09 15:27:10 +0800

    删除出现bug代码

---
+++ assets/scripts/core/util/GameEnumValue.js
@@ -0,0 +1,87 @@
+module.exports = {
+    StaffType: {
+        Seller: 1,// 销售
+        Finance: 2,//财务
+        Operation: 3,//运营
+    },
+    ShopStatus: {
+        None: 0,// 空状态
+        UnLock: 1,// 未解锁
+        WaitCollect: 2,// 等待收集
+        WaitBusiness: 3,// 等待营业
+        Busyness: 4,// 营业中
+    },
+
+    ClientType: {
+        Web: 1,
+        Android: 2,
+        Ios: 3,
+        Other: 4,
+    },
+    Page: {
+        DialogSure:72,
+        MainScene: 1,
+        CarShop: 2,
+        EnterShop3:3,
+        UnlockShop4:4,
+        GotoShop5:5,
+        LockPage6:6,
+        Deblocking7:7,
+        LockSuccess8:8,
+        Park9:9,
+        BuyCar10:10,
+        BuySale11:11,
+        ClickHireSale005:115,
+        BuySaleYes12:12,
+        StarExtendApart13:13,
+        GotoApartMent14:14,
+        ArriveApert15:15,
+        Upgrade151:15.1,
+        StartExtendApart152:15.2,//点击扩建宿舍进行扩建
+        AgainBuyEmp16:16,
+        HireEmp17:17,//太棒了！宿舍现在可以容纳2位员工了，跟娜娜去店铺雇佣员工去吧！
+        ApartMentBack18:18,
+        NextGotoShop19:19,
+        NextGotoCarDetail20:20,
+        HireFinance22:22,
+        BuyFinaanceYes23:23,
+        UpDateShop24:24,
+        UpDateShopYes25:25,
+        StartBusiness26:26,
+        StartBusinessYes27:27,
+        GetGold28:28,
+        AgainCenterShopList29:29,
+        ShopLevel30:30,
+        NextEnterMain31:31,
+        EnterTask32:32,
+        FirstStepSuccess:33,
+
+
+        BankGuidance40:40,
+        MasterGuid50:50,
+        MasterGuid51:51,
+        ReputationGuid60:60,
+        TreeGuidance70:70,
+        FriendGuid80:80,
+        FriendGuid81:81,
+        FriendGuid82:82,
+        FriendGuid83:83,
+        FriendGuid84:84,
+
+        RankGuid90:90,
+        RankGuid91:91,
+        RankGuid92:92,
+        TreeGuid100:100,
+
+        TradeGuid120:120,
+        TradeGuid121:121,
+        TradeGuid122:122,
+        TradeGuid123:123,
+
+        //临时添加
+        task:15.3,
+        HireDialogSure:15.4,
+    },
+
+
+};
---
+++ assets/scripts/core/util/SettingModule.js
@@ -0,0 +1,6 @@
+module.exports={
+    OnUpdateVolume:"SettingModule_OnUpdateVolume",// 更新音量
+
+    OnEffectiveMusic:"SettingModule_OnEffectiveMusic",
+    MusicObj:"SettingModule_MusicObj"
+};
